e3f0e5a3b9ab2d68b22ba0d7ed9943cc6226b7ea
Maksim Berdnikau <max.berd.1900@gmail.com>
2024-11-22 17:58:50 +0300

    Исправление

--- Infrastructure/PerformanceRatingSystem.Infrastructure/EmployeePerformanceContext.cs
+++ Infrastructure/PerformanceRatingSystem.Infrastructure/EmployeePerformanceContext.cs
@@ -10,25 +10,17 @@ namespace PerformanceRatingSystem.Infrastructure;
 public partial class EmployeePerformanceContext(DbContextOptions<EmployeePerformanceContext> options) 
     : IdentityDbContext<User>(options)
 {
-
     public virtual DbSet<Achievement> Achievements { get; set; }
-
     public virtual DbSet<ActualPerformanceResult> ActualPerformanceResults { get; set; }
-
     public virtual DbSet<Department> Departments { get; set; }
-
     public virtual DbSet<DepartmentPerformanceIndicator> DepartmentPerformanceIndicators { get; set; }
-
     public virtual DbSet<Employee> Employees { get; set; }
-
     public virtual DbSet<EmployeePerformanceIndicator> EmployeePerformanceIndicators { get; set; }
-
     public virtual DbSet<PlannedPerformanceValue> PlannedPerformanceValues { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
 
         modelBuilder.ApplyConfiguration(new DepartmentConfiguration());
     }
